cd4f2e5ec4e1765c5eb04a43f2ccccab8e7e5aa5
ferstrak <fernandobrana.r@gmail.com>
2026-07-02 13:43:59 -0400

    Docker operador

--- src/main/java/cl/example/dronechile_orquestador/config/WebClientConfig.java
+++ src/main/java/cl/example/dronechile_orquestador/config/WebClientConfig.java
@@ -11,37 +11,37 @@ public class WebClientConfig {
     // Cada @Bean es como un "enchufe" listo para ser usado por el sistema
 
     @Bean(name = "pilotoWebClient")
-    public WebClient pilotoWebClient(@Value("${api.pilotos.url:http://localhost:8086/api/pilotos}") String url) {
+    public WebClient pilotoWebClient(@Value("${api.pilotos.url:https://piloto-dfcf.onrender.com}") String url) {
         return WebClient.builder().baseUrl(url).build();
     }
 
     @Bean(name = "licenciaWebClient")
-    public WebClient licenciaWebClient(@Value("${api.licencias.url:http://localhost:8085/api/licencias}") String url) {
+    public WebClient licenciaWebClient(@Value("${api.licencias.url:https://licencia-bvee.onrender.com}") String url) {
         return WebClient.builder().baseUrl(url).build();
     }
 
     @Bean(name = "vueloWebClient")
-    public WebClient vueloWebClient(@Value("${api.registros.url:http://localhost:8089/api/registros}") String url) {
+    public WebClient vueloWebClient(@Value("${api.registros.url:https://registrovuelo.onrender.com}") String url) {
         return WebClient.builder().baseUrl(url).build();
     }
 
     @Bean(name = "solicitudWebClient")
-    public WebClient solicitudWebClient(@Value("${api.solicitudes.url:http://localhost:8080/api/solicitudes}") String url) {
+    public WebClient solicitudWebClient(@Value("${api.solicitudes.url:https://solicitudes-e4e1.onrender.com}") String url) {
         return WebClient.builder().baseUrl(url).build();
     }
 
     @Bean(name = "tipoTrabajoWebClient")
-    public WebClient tipoTrabajoWebClient(@Value("${api.tipostrabajo.url:http://localhost:8083/api/tipos-trabajo}") String url) {
+    public WebClient tipoTrabajoWebClient(@Value("${api.tipostrabajo.url:https://tipotrabajo.onrender.com}") String url) {
         return WebClient.builder().baseUrl(url).build();
     }
 
     @Bean(name = "dronWebClient")
-    public WebClient dronWebClient(@Value("${api.drones.url:http://localhost:8082/api/drones}") String url) {
+    public WebClient dronWebClient(@Value("${api.drones.url:https://drones1-1.onrender.com}") String url) {
         return WebClient.builder().baseUrl(url).build();
     }
 
     @Bean(name = "empresaWebClient")
-    public WebClient empresaWebClient(@Value("${api.empresas.url:http://localhost:8081/api/empresas-proveedoras}") String url) {
+    public WebClient empresaWebClient(@Value("${api.empresas.url:https://empresaproveedora1.onrender.com}") String url) {
         return WebClient.builder().baseUrl(url).build();
     }
 }
